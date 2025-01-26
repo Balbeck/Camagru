8dd51f11a947367c8d7e898e98fa1dc628f98d74
HectorAlbecker <hector.albecker@sanofi.com>
2025-01-26 18:52:47 +0100

    edit: index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title> 🗺️ Hello World 🌍 </title>
+    </head>
+
+    <body>
+        <h1> Test Pour PHP </h1>
+        <p>
+            <?php
+                echo 'Hello Docker !';
+                
+            ?>
+        </p>
+    </body>
+
+</html>
