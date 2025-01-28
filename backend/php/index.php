5cf728de32d06cf83932213adc3e246c20479fba
HectorAlbecker <hector.albecker@sanofi.com>
2025-01-28 02:13:09 +0100

    edit: config for API routing

--- backend/php/index.php
+++ backend/php/index.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-    <head>
-        <title> 🗺️ Hello World 🌍 </title>
-    </head>
-
-    <body>
-        <h1> Test Pour PHP </h1>
-        <p>
-            <?php
-                echo 'Hello Docker ! 🐳 ';
-                
-            ?>
-        </p>
-    </body>
-
-</html>
+<?php
+require_once 'routes/api.php';
+?>
