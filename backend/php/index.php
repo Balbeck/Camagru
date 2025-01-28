4f528ebd857169728096ff54d300f1520e71c77c
HectorAlbecker <hector.albecker@sanofi.com>
2025-01-28 02:30:20 +0100

    fix: try fix cors

--- backend/php/index.php
+++ backend/php/index.php
@@ -1,3 +1,7 @@
 <?php
-require_once './routes/api.php';
-?>
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: http://localhost:3000"); // Ajustez selon votre configuration frontend
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+require_once 'routes/api.php';
